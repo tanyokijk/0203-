a9b2fd80730a370a5d09f4f67c2001d52404c6ac
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2023-10-22 16:31:40 +0300

    Добавьте файлы проекта.

---
+++ 0203 один потік з параметрами/0203 один потік з параметрами.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <thread>
+
+void displayLetters(int a, int b)
+{
+    std::string alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+    for (int i = a - 1; i < b; i++)
+    {
+        std::cout << alphabet[i] << " ";
+    }
+}
+
+int main()
+{
+    system("chcp 1251>null");
+    int first=0, last=0;
+    while (first <= 0 || first>33 ||last <= 0 || last>33)
+    {
+        std::cout << "Введіть діапазон букв (спочатку верхню межу і, через натискання Enter, нижню)" << std::endl;
+        std::cin >> first >> last;
+        if (first <= 0 || first > 33)
+        {
+            std::cout << "Помилка. Ви ввели недопустиму верхню межу!"<<std::endl;
+        }
+        if (last <= 0 || last > 33)
+        {
+            std::cout << "Помилка. Ви ввели недопустиму нижню межу!" << std::endl;
+        }
+    }
+    std::thread t(displayLetters, first,last);
+    t.join();
+    std::cout << std::endl << "Головний потік закінчив виконання" << std::endl;
+
+}
